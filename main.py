127f46c9ff331620c3528f3566a6949e819c61f2
Setlerr <86960769+Setlerr@users.noreply.github.com>
2022-06-04 23:20:12 +0200

    logs to one channel function

--- main.py
+++ main.py
@@ -1,24 +1,72 @@
 import discord
 import os 
 
+
 from discord.ext import tasks, commands
 from discord.utils import get
 
-client = commands.Bot(command_prefix='`')
+#import token from file
+from apikeys import *
 
-@client.event
-async def on_ready():
-    print("Bot is working")
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+
+client = commands.Bot(command_prefix='.', intents=intents) #define prefix
+
 
 
 @client.command() #define commands
 async def hello(ctx):
     await ctx.send("Hello, I'm still working!")
 
+@client.command()
+async def hello1(ctx):
+    await ctx.send("Hello, I'm still working1!")
+
+@client.command()
+async def hello2(ctx):
+    await ctx.send("Hello, I'm still working2!")
+
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(982713233758634004) #channel id where message will be sent
+    await channel.send(f"Witaj {member}")    
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(982713233758634004)
+    await channel.send(f"Spierdolił {member}")
+
+@client.command(pass_context=True)
+async def join(ctx):
+    if(ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("Wejdź na kanał głosowy!")
+
+#guild is basiclly your server so, go to guild > voice channel > leave
+#Learning how joining works
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()#End of this
+
+#check for word learning
+
+#all messege logs
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    channel = client.get_channel(982746762303402014) #channel id where message will be sent    
+    await channel.send(message.content)
+    await channel.send("#"+message.channel)
+    print(message.content)
 
 
 
-
-
-
-client.run('TOKEN')
+client.run(TOKEN)
