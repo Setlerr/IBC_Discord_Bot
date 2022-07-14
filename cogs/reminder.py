7d498c54d7d88376e96991c3a0f15da38f3bc057
Setlerr <86960769+Setlerr@users.noreply.github.com>
2022-07-14 12:25:57 +0200

    Ability to remind (ping) people for event

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,36 +1,47 @@
 from email import message
 import numbers
 import nextcord
+import nextcord.utils
 import linecache as lc
 
 from nextcord.ext import tasks, commands
-from nextcord.utils import get
-from datetime import datetime
-from time import gmtime, strftime
-from array import array
 
 intents = nextcord.Intents.all()
 intents.members = True
 intents.messages = True
 
-def has_numbers(inputString):
-    return any(char.isdigit() for char in inputString)
 class Reminder(commands.Cog):
     
     def __init__(self,client):
         self.client=client
-#remind
-    @commands.Cog.listener()
-    async def re(self, ctx):
-        await ctx.channe.send("remind")#to delete  
+#register
+    @commands.command(name='reminder',aliases=['powiadom','alert','remind','przypomnij'])
+    async def remind(self, ctx): 
+        await ctx.message.delete()
+        if ctx.author.guild_permissions.administrator:
+            messages = await ctx.channel.history(limit=400).flatten()
+            users_in_message = []
+            users_to_ping = "Przypominam o misji!\n"
+            for event in messages:
+                if "Event" in event.content:
+                    mess = event.content
+                    mess = mess.replace("**","")
+                    print(mess)
+                    mess = mess.split()
+                    for word in mess:
+                        if nextcord.utils.get(ctx.guild.members, name=word) != None:
+                            user = nextcord.utils.get(ctx.guild.members, name=word)
+                            users_in_message.append(user.mention)
+                            print(user)
+                    for user in users_in_message:
+                        users_to_ping += str(f"{user}")
+                    await ctx.channel.send(content = users_to_ping)
+        else:
+            await ctx.channel.send("Nie jesteś Administratorem!")
+
+
+                
+
 
 def setup(client):
     client.add_cog(Reminder(client))
-
-
-
-
-
-
-
-# @client.command()
